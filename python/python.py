4b981e3551e7c9e248551c4227a713cf2aa9f55e
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-05-25 18:46:51 -0600

    if statement

--- python/python.py
+++ python/python.py
@@ -4,10 +4,22 @@ bo = False
 
 print(bo)
 
-y = 5
+y = 15
 
 print (y)
 
 nums = [1,2,3]
 
 print (nums)
+
+
+if(y < -0):
+    print('y is negative')
+elif(y >= 15 and bo == False):
+    print('big and false')
+elif(y < 5 and bo == True):
+    print('small and true')
+elif(y >= 100 or bo == True):
+    ('large or true')
+else:
+    print('i dont know')    
